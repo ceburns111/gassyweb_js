480763cfdeec1b9eef759701b26b99b67695c7af
Christopher (Ted) Burns <ceburns111@gmail.com>
2022-06-13 13:22:29 -0400

    feat: finished bare bones listings view

--- src/views/ListingsView.vue
+++ src/views/ListingsView.vue
@@ -9,6 +9,11 @@ export default {
   },
   listingService: null,
   methods: {},
+  computed: {
+    listingKeys() {
+      return Object.keys(listings[0]);
+    }
+  },
   created() { 
     this.listingService = new ListingService();
   },
@@ -19,7 +24,11 @@ export default {
 </script>
 
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <li>{{listing.make}}</li>
-  </div>
+  <table>
+    <tr v-for="listing in listings" :key="listing.id">
+      <td v-for="(value) in listing" :key="listing.value">
+        {{value}}
+      </td>
+    </tr>
+  </table>
 </template>
