b617d98495c027e84a86d713e39ef406fcb721e6
Christopher (Ted) Burns <ceburns111@gmail.com>
2022-07-22 14:29:04 -0400

    feat: current ui elements functionally complete

---
+++ src/components/WelcomeToGassy.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data () {
+      return {
+    }
+  },
+};
+</script>
+
+<template>
+<h1>Welcome to Gassy!</h1>
+</template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
-<script setup>
+<script>
+import WelcomeToGassy from '@/components/WelcomeToGassy.vue';
+export default {
+    name: "home",
+    data() {
+        return {};
+    },
+    components: { WelcomeToGassy }
+}
 </script>
 
 <template>
-    <h2>Do you have G.A.S?</h2>
-    <p class="about_par">G.A.S: Get another synth! Duh.</p>
-    <br>
-    <br>
-    <p class="about_par">
-        If youre a synth nerd like me, you know this acronym all to well. 
-        Well, I've got the cure. Meet Gassy. An aggregate e-commerce search tool intended to help you get the 
-        "hey reddit check out this cs-80 i found in a dumpster near my house is it worth something" type deals. 
-    </p>
+<main>
+  <WelcomeToGassy></WelcomeToGassy>
+</main>
 </template>
 
 <style>
+
 </style>
 
