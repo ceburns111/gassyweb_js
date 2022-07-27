f076b9a7cfda830bdc31bbf4586ee33217561ec1
Christopher (Ted) Burns <ceburns111@gmail.com>
2022-07-26 23:56:35 -0400

    feat: added delete account, refactored signup

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -24,8 +24,8 @@ export default {
     async submitEdit() {
        await accountService.editAccount(this.account.id, this.account.firstName, this.account.lastName, this.account.email, "");
     },
-    deleteAccount() {
-        const x = "";
+    async deleteAccount() {
+        await accountService.deleteAccount(this.account.id);
     }
    }
 }
